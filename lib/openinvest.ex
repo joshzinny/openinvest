3c54e4e1555a8a4dd42627a57b8742c3f1e13f62
joshzinny <joshzinny@yahoo.com>
2017-10-30 19:24:52 +0000

    First commit

---
+++ lib/openinvest.ex
@@ -0,0 +1,18 @@
+defmodule Openinvest do
+  @moduledoc """
+  Documentation for Openinvest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Openinvest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/openinvest_test.exs
@@ -0,0 +1,8 @@
+defmodule OpeninvestTest do
+  use ExUnit.Case
+  doctest Openinvest
+
+  test "greets the world" do
+    assert Openinvest.hello() == :world
+  end
+end
